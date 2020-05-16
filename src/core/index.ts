ab4204f1e3943e54036054bf668e40f7261ab20a
wanho1108 <wanho1108@gmail.com>
2020-05-16 22:26:24 +0900

    jQuery.init 개선

--- src/core/index.ts
+++ src/core/index.ts
@@ -6,18 +6,27 @@ jQuery.fn = jQuery.prototype = {
   version: "1.0",
 };
 
-jQuery.fn.init = function (selector, context) {
-  let elements;
-
-  this.context = context instanceof Element ? context : document;
-
-  if (typeof selector === "string") {
-    elements = this.context.querySelectorAll(selector);
+jQuery.fn.init = function (
+  selector: string | object | undefined,
+  context: HTMLElement | undefined
+) {
+  if (!selector) {
+    return this;
   }
 
-  if (elements) {
+  if (typeof selector === "string") {
+    let elements: NodeList;
+
+    this.context = context instanceof Element ? context : document;
+    elements = this.context.querySelectorAll(selector);
     Array.prototype.forEach.call(elements, (element, i) => (this[i] = element));
     this.length = elements.length;
+  } else if (Array.isArray(selector)) {
+    selector.forEach((item, i) => (this[i] = item));
+    this.length = selector.length;
+  } else if (typeof selector === "object") {
+    this[0] = selector;
+    this.length = 1;
   }
 };
 
