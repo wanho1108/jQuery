dae1f7eb795015f87aa81b84809896485f9cac6c
wanho1108 <wanho1108@gmail.com>
2020-06-24 00:55:20 +0900

    Core 개선중

--- src/core/index.ts
+++ src/core/index.ts
@@ -1,33 +1,34 @@
+import attributes from "../attributes";
+
 const jQuery: JQuery = (selector: string, context: HTMLElement) => {
   return new jQuery.fn.init(selector, context);
 };
 
 jQuery.fn = jQuery.prototype = {
   version: "1.0",
+  ...attributes,
 };
 
 jQuery.fn.init = function (
   selector: string | object | undefined,
   context: HTMLElement | undefined
 ) {
-  if (!selector) {
-    return this;
-  }
-
-  if (typeof selector === "string") {
-    let elements: NodeList;
-
-    this.context = context instanceof Element ? context : document;
-    elements = this.context.querySelectorAll(selector);
-    Array.prototype.forEach.call(elements, (element, i) => (this[i] = element));
-    this.length = elements.length;
-  } else if (Array.isArray(selector)) {
-    selector.forEach((item, i) => (this[i] = item));
-    this.length = selector.length;
-  } else if (typeof selector === "object") {
-    this[0] = selector;
-    this.length = 1;
-  }
+  //   if (!selector) {
+  //     return this;
+  //   }
+  //   if (typeof selector === "string") {
+  //     let elements: NodeList;
+  //     this.context = context instanceof Element ? context : document;
+  //     elements = this.context.querySelectorAll(selector);
+  //     Array.prototype.forEach.call(elements, (element, i) => (this[i] = element));
+  //     this.length = elements.length;
+  //   } else if (Array.isArray(selector)) {
+  //     selector.forEach((item, i) => (this[i] = item));
+  //     this.length = selector.length;
+  //   } else if (typeof selector === "object") {
+  //     this[0] = selector;
+  //     this.length = 1;
+  //   }
 };
 
 export default jQuery;
--- src/jQuery.ts
+++ src/jQuery.ts
@@ -1,3 +1,3 @@
-import core from './core';
+import core from "./core";
 
 window.jQuery = core;
